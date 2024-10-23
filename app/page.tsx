6bc9b184ae1cda954eb33d6b94925b9d96d97bf9
Sahan <ks.official.sahan@gmail.com>
2024-10-23 16:31:02 +0530

    Portforlio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import IMAGINECOREX_ICON from "@/components/icons/ImaginecoreX";
 import VIP_ICON from "@/components/icons/VIP";
 import Particles from "@/components/magicui/particles";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
@@ -14,6 +13,7 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { RubberBandElement } from "@/components/animations/shoelace/rubber-band";
 import Explore from "@/components/sections/Explore";
+import EVISIONIT_ICON from "@/components/icons/EvisionIT";
 
 export default function Home() {
   const { theme } = useTheme();
@@ -112,14 +112,14 @@ export default function Home() {
                   className="w-[268px] min-h-[220px] pt-[30px] pb-[20px] rounded-[24px] border bg-[#f7f7f7] dark:bg-[#00000052] flex flex-col items-center justify-center box-border px-[25px]"
                 >
                   <div className="w-[80px] h-[80px] rounded-full bg-black dark:bg-white flex justify-center items-center pt-[3px]">
-                    <IMAGINECOREX_ICON
+                    <EVISIONIT_ICON
                       className="fill-white dark:fill-black"
-                      width={50}
-                      height={17}
+                      width={60}
+                      height={30}
                     />
                   </div>
                   <div className="text-[10px] font-medium opacity-65 pt-[14px]">
-                    Founder & CEO of ImaginecoreX
+                    Founder & CEO of Evision IT
                   </div>
 
                   <a
@@ -128,14 +128,14 @@ export default function Home() {
                     rel="noopener noreferrer"
                     className="w-full"
                   >
-                    <Button className="mt-[20px] justify-start h-[40px] w-full border rounded-full p-[4px] flex items-center gap-[10px] bg-[#ffffff75] dark:bg-[#00000075] backdrop-blur-sm">
+                    <Button className="mt-[20px] justify-start text-center h-[40px] w-full border rounded-full p-[4px] flex items-center gap-[10px] bg-[#ffffff75] dark:bg-[#00000075] backdrop-blur-sm">
                       <div className="p-[8px] flex justify-center items-center border bg-[#f7f7f756] dark:bg-[#23232356] backdrop-blur-sm rounded-full w-fit h-fit">
                         <VIP_ICON className="fill-[#FFB84D]" />
                       </div>
                       <span
-                        className={`${righteous.className} text-[14px] bg-imcroxBro bg-clip-text text-transparent`}
+                        className={`${righteous.className} text-[14px] bg-imcroxBro text-center w-full bg-clip-text text-transparent`}
                       >
-                        www.imaginecorex.com
+                        www.evisionit.lk
                       </span>
                     </Button>
                   </a>
